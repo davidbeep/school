868f1a59e3231c4f81c0c3108f0c1314125acbb9
DanielG6408 <danielg6408@gmail.com>
2015-07-27 22:39:07 -0400

    Added high scores in options menu

---
+++ Sir Umbrella/Assets/HighScoreBehavior.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighScoreBehavior : MonoBehaviour
+{
+
+	public Text level1_1;
+	public Text level1_2;
+	public Text level1_3;
+	public Text level2_1;
+	public Text level2_2;
+	public Text level2_3;
+	// Use this for initialization
+	void Start ()
+	{
+		level1_1.text = "";
+		level1_2.text = "";
+		level1_3.text = "";
+		level2_1.text = "";
+		level2_2.text = "";
+		level2_3.text = "";
+
+		if (GameController.controller.getHighScore (0) != 0) {
+			level1_1.text = "Level 1-1: " + GameController.controller.getHighScore (0);
+		} else {
+			level1_1.text = "None";
+		}
+		if (GameController.controller.getHighScore (1) != 0) {
+			level1_2.text = "Level 1-2: " + GameController.controller.getHighScore (1);
+		}
+		if (GameController.controller.getHighScore (2) != 0) {
+			level1_3.text = "Level 1-3: " + GameController.controller.getHighScore (2);
+		}
+		if (GameController.controller.getHighScore (3) != 0) {
+			level2_1.text = "Level 2-1: " + GameController.controller.getHighScore (3);
+		}
+		if (GameController.controller.getHighScore (4) != 0) {
+			level2_2.text = "Level 2-2: " + GameController.controller.getHighScore (4);
+		}
+		if (GameController.controller.getHighScore (5) != 0) {
+			level2_3.text = "Level 2-3: " + GameController.controller.getHighScore (5);
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
--- Sir Umbrella/Assets/Scripts/Options.cs
+++ Sir Umbrella/Assets/Scripts/Options.cs
@@ -2,65 +2,75 @@
 using System.Collections;
 using UnityEngine.UI ;
 
-public class Options : MonoBehaviour 
+public class Options : MonoBehaviour
 {
 
 	//GameObject musicButton ;
 	public Button musicButton ;
 	public Button sfxButton ;
 	public Sprite green, red ;
+	public Button highScoreButton;
+	private GameObject highScores;
+	private bool highScoreToggle;
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
-		if (MusicManager.instance.IsPlaying() == false) 
-		{
-			musicButton.image.sprite = red ;
+		if (MusicManager.instance.IsPlaying () == false) {
+			musicButton.image.sprite = red;
 		}
-		if (MusicManager.instance.SoundEffectsOn() == false) 
-		{
-			sfxButton.image.sprite = red ;
+		if (MusicManager.instance.SoundEffectsOn () == false) {
+			sfxButton.image.sprite = red;
 		}
+
+		highScoreToggle = false;
+		highScores = GameObject.FindGameObjectWithTag ("highScores");
+		highScores.SetActive (false);
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
-	
+		if (!highScoreToggle) {
+			highScoreButton.image.sprite = green;
+		} else {
+			highScoreButton.image.sprite = red;
+		}
 	}
 
-	public void toggleBGMusic()
+	public void toggleBGMusic ()
 	{
-		if ( MusicManager.instance.IsPlaying() ) 
-		{
+		if (MusicManager.instance.IsPlaying ()) {
 			MusicManager.instance.Pause ();
 			// change button image color now to red (should be green to start)
-			musicButton.image.sprite = red ;
+			musicButton.image.sprite = red;
 
-		}
-		else 
-		{
-			MusicManager.instance.UnPause() ;
+		} else {
+			MusicManager.instance.UnPause ();
 			// change button image color now to green
-			musicButton.image.sprite = green ;
+			musicButton.image.sprite = green;
 		}
 
 	}
-	public void toggleSoundEffects()
+	public void toggleSoundEffects ()
 	{
-		if (MusicManager.instance.SoundEffectsOn ()) 
-		{
-			MusicManager.instance.DisableSoundEffects() ;
-			sfxButton.image.sprite = red ;
-		}
-		else 
-		{
-			MusicManager.instance.EnableSoundEffects() ;
-			sfxButton.image.sprite = green ;
+		if (MusicManager.instance.SoundEffectsOn ()) {
+			MusicManager.instance.DisableSoundEffects ();
+			sfxButton.image.sprite = red;
+		} else {
+			MusicManager.instance.EnableSoundEffects ();
+			sfxButton.image.sprite = green;
 		}
 	}
 
-	public static void resetScore(){
-		GameController.controller.resetScores();
+	public void toggleHighScores ()
+	{
+		highScoreToggle = !highScoreToggle;
+		if (!highScores.activeSelf) {
+			highScores.SetActive (true);
+
+		} else {
+			highScores.SetActive (false);
+		}
 	}
 }
